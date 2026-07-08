63d32abc790b1f37a17f79412ba969df93a4f48a
shivaprakash2707-code <shivaprakash2707@gmail.com>
2026-07-08 16:54:04 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://shivaprakash2707-code.github.io/fullstackbackend/";
 
 // --- Session Guard ---
 const token = localStorage.getItem("token");
